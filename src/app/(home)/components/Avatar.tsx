4145c17b2559ca8ae6490386ccf3165eba1849db
Mehdi4556 <mehdim12425@gmail.com>
2025-11-21 17:40:55 +0500

    fix the avatar

--- src/app/(home)/components/Avatar.tsx
+++ src/app/(home)/components/Avatar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Avatar = () => {
   return (
-    <Link href='https://x.com/intent/follow?screen_name=fardeentwt' target='_blank' className="block">
+    <Link href='https://www.linkedin.com/in/muhammad-mehdi-53952a360/' target='_blank' className="block">
       <div className="relative w-40 h-40 rounded-full overflow-hidden">
         <Image
           src='/hero.jpg'
